27132e87ee59a5c36eeb00da6a56879716f33309
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-14 10:29:46 +1030

    general rules of hooks

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 // useState - function
+// component name must be uppercase
+// must be in the function/ component body
+// cannot call conditionally
 
 const UseStateBasics = () => {
   // console.log(useState("hello world"));
@@ -7,6 +10,8 @@ const UseStateBasics = () => {
   // const handler = useState(1)[1];
   // console.log(value, handler);
   const [text, setText] = useState("random title");
+  // text is the variable, and setText is the variable used to control the state of the
+  // variable when handleClick is called
 
   const handleClick = () => {
     if (text === "random title") {
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+// importing data from few levels up
+import { data } from "../../../data";
 
 const UseStateArray = () => {
+  // can either import { useState } or use React.useState
+  const [people, setPeople] = React.useState([]);
   return <h2>useState array example</h2>;
 };
 
