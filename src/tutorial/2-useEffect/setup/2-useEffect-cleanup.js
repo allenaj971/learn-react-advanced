35d82876963b5a8c3f53769a97372cf6da2919d3
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-14 13:25:49 +1030

    useEffect - cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    // func determines the new page width
+    setSize(window.innerWidth);
+  };
+  // here we here are checking for when the window resizes, and call the checkSize
+  // function to assign the value of setSize
+  // useEffect is a function, that when executed, has a side-effect. i.e. when page is
+  // resized, the side-effect is that the size value has the updated page width value.
+  // it is good practice that when you set up an useEffect side effect, you set up a
+  // cleanup function that removes additional eventlisteners for example. In this case,
+  // we set dependency so that it is only executed on initial render.
+  useEffect(() => {
+    console.log("useEffect");
+    window.addEventListener("resize", checkSize);
+    // this below function removes the addition event listeners created
+    // return () => {
+    //   console.log("cleanup");
+    //   window.removeEventListener("resize", checkSize);
+    // };
+  }, []);
+
+  console.log("render");
+
+  return (
+    <>
+      <h1>windows</h1>
+      <h2>width: {size}</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
