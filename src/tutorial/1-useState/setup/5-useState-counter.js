bda1f05951482da0c9223eecb184e2bc2f87250b
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-14 11:11:21 +1030

    simple counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  return (
+    // <> is React.fragment rather than using div
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Counter</h2>
+        <h1>{value}</h1>
+        {/* rather than defining a function outside, just using inline function to 
+        set the value of value */}
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          -
+        </button>
+        <button className="btn" onClick={() => setValue(0)}>
+          reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          +
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
