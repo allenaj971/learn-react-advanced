5aa8a9bef154e628dcce77cc54bc5791ccb97607
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-14 11:25:24 +1030

    functional update form

--- src/tutorial/1-useState/final/3-useState-array.js
+++ src/tutorial/1-useState/final/3-useState-array.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
 
@@ -7,18 +7,19 @@ const UseStateArray = () => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
+
   return (
     <>
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button className='btn' onClick={() => setPeople([])}>
+      <button className="btn" onClick={() => setPeople([])}>
         clear items
       </button>
     </>
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,6 +3,18 @@ import React, { useState } from "react";
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
 
+  const delayIncrease = () => {
+    // using the setTimeout function to setValue to increase after 2 seconds/200 milliseconds
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevValue) => {
+        // the setValue function stores the previous value and adds one so even if we click
+        // the delayed counter 4 times, it will add 4 to the current value
+        return prevValue + 1;
+      });
+    }, 2000);
+  };
+
   return (
     // <> is React.fragment rather than using div
     <>
@@ -18,6 +30,15 @@ const UseStateCounter = () => {
           reset
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
+          +
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Delayed Counter</h2>
+        <h1>{value}</h1>
+        {/* rather than defining a function outside, just using inline function to 
+        set the value of value */}
+        <button className="btn" onClick={delayIncrease}>
           +
         </button>
       </section>
