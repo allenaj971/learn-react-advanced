d3163c90f0ab9f6aba3d5edca2b1a0e9d43d4f72
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-14 11:00:00 +1030

    useState - Array and Object example

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -4,8 +4,40 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   // can either import { useState } or use React.useState
-  const [people, setPeople] = React.useState([]);
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  // remove item function
+  const removeItem = (id) => {
+    // filter creates a new array that satisfies the test implemented
+    // (in this case find matching id to remove it by setting newPeople to id and name
+    // that doesn't match the id passed in)
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        // deconstructs & maps each element id and name to person
+        const { id, name } = person;
+        return (
+          // we need to pass in the id to ensure unique key prop when referenced
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+          // the .map loops over each element so that it displays all items in the list
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        {/* we set up arrow func to avoid calling function while page loads, but only when
+        we click the button. To clear all items, setPeople is passed an empty list */}
+        clear items
+      </button>
+      <button className="btn" onClick={() => setPeople(data)}>
+        get items back
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // here the initial object person
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: "24",
+    message: "random message",
+  });
+  // console.log(person);
+
+  const changeMessage = () => {
+    // when setting the value here, it will change the object to a string and wipe out other values
+    // setPerson("hello world");
+    // to solve the issue we use seperator thing to tell javascript to leave those values
+    // and tell it to modify the required attribute.
+    setPerson({ ...person, message: "hello World" });
+  };
+
+  return (
+    <div>
+      {/* to reference the person dictionary, use .'attribute' */}
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
