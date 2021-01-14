dd62cafb4a4884a6754b80d3b069349efaedde3c
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-14 11:05:23 +1030

    useState - multiple state values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -9,20 +9,26 @@ const UseStateObject = () => {
   });
   // console.log(person);
 
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
   const changeMessage = () => {
     // when setting the value here, it will change the object to a string and wipe out other values
     // setPerson("hello world");
-    // to solve the issue we use seperator thing to tell javascript to leave those values
+    // to solve the issue we use spread thing to tell javascript to leave those values
     // and tell it to modify the required attribute.
     setPerson({ ...person, message: "hello World" });
+    // you could seperately deconstruct and useState the values but up to you
+    // setMessage("hello World");
   };
 
   return (
     <div>
       {/* to reference the person dictionary, use .'attribute' */}
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className="btn" onClick={changeMessage}>
         change message
       </button>
