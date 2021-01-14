2b38ac0d9853b60a7d1ba8a7f57050363822d5ce
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-15 10:26:07 +1030

    useRef

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,40 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-// preserves value
+// preserves value between renders
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+  const handleSubmit = (e) => {
+    // prevents page from reloading
+    e.preventDefault();
+    // prints out the current value of the container.
+    console.log(refContainer.current.value);
+    // the containers points to the value inside the div (in this case hello world)
+    console.log(divContainer.current);
+  };
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          {/* the useRef is getting the input data by referencing the container
+          by ref={refContainer}. */}
+          <input type="text" ref={refContainer} />
+          <button type="submit">submit</button>
+        </div>
+      </form>
+      <div ref={divContainer}>hello world</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
