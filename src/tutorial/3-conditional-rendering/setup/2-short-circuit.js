7c14114ae89df03a91877ae5bf6f871168e9ac58
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-14 15:13:06 +1030

    short-circuit evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h2>value: {secondValue}</h2> */}
+      {/* {if(){console.log('hello world')}} */}
+      {/* if we want to return some default value if none exists, use || (or operator) */}
+      <h1>{text || "john doe"}</h1>
+      {/* if text is false, return hello world */}
+      {text && <h1>hello world</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
