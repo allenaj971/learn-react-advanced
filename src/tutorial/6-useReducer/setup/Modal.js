486fa63914fc6625c8e55a4f3cd28362dc10129e
sciencetechrocks <sciencetechrocks@gmail.com>
2021-01-15 10:47:46 +1030

    useReducer - useState setup

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const Modal = () => {
-  return <div>i'm modal</div>;
+  return <p>i'm modal</p>;
 };
 
 export default Modal;
